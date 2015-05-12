d3da15a8f779e92f42ce99b7bcc0148fc12838c2
lsp <spieled916@gmail.com>
2015-05-12 14:15:20 +0800

    change module scorp-web to scorp-webapp

---
+++ scorp-webapp/src/main/java/com/studease/scorp/servlet/HelloServlet.java
@@ -0,0 +1,42 @@
+package com.studease.scorp.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Author: liushaoping
+ * Date: 2015/5/12.
+ */
+public class HelloServlet implements Servlet {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+        System.out.println("HelloServlet is in processing ....");
+        servletResponse.getWriter().append("HelloServlet is in processing ....").flush();
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
